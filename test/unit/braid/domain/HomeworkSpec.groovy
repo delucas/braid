d3354bbf4dd83fe024d4d548c23c75da4c83bb2a
Lucas Videla <videla.lucas@gmail.com>
2013-02-09 01:23:28 -0300

    Se agrega spock test sobre Homework

---
+++ test/unit/braid/domain/HomeworkSpec.groovy
@@ -0,0 +1,42 @@
+package braid.domain
+
+import grails.test.mixin.*
+import grails.test.mixin.support.GrailsUnitTestMixin
+import spock.lang.Specification
+import braid.DateService;
+import braid.Homework
+
+/**
+ * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
+ */
+@TestMixin(GrailsUnitTestMixin)
+class HomeworkSpec extends Specification {
+
+	def homework
+	def now
+	
+	void setup() {
+		now = new Date()
+		
+		homework = new Homework()
+		homework.dateService = [currentTime: now]
+	}
+	
+	void "when dueDate after now then is not outOfDate"() {
+		
+		def tomorrow = now + 1
+		when:
+			homework.dueDate = tomorrow
+		then:
+			!homework.outOfDate
+	}
+	
+	void "when dueDate before now then is outOfDate"() {
+		
+		def yesterday = now - 1
+		when:
+			homework.dueDate = yesterday
+		then:
+			homework.outOfDate
+	}
+}
