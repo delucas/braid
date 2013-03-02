474c5ad351344427b7b8a4154120d2063ec394b4
Matias Gonzalez <mgon.uat@gmail.com>
2013-03-02 13:40:09 -0300

    se integra braid con github

---
+++ grails-app/services/oauth/GithubAuthService.groovy
@@ -0,0 +1,38 @@
+package oauth
+
+import grails.converters.deep.JSON
+import org.scribe.model.OAuthRequest
+import org.scribe.model.Token
+import org.scribe.model.Verb
+import org.scribe.model.Verifier
+import org.scribe.oauth.OAuthService
+
+class GithubAuthService extends GrailsOAuthService {
+
+    @Override
+    OAuthService createOAuthService(String callbackUrl) {
+        return createServiceBuilder(GitHubApi,
+                    grailsApplication.config.auth.github.key as String,
+                    grailsApplication.config.auth.github.secret as String,
+                    callbackUrl).build()
+    }
+
+    AuthInfo getAuthInfo(String callbackUrl) {
+        OAuthService authService = createOAuthService(callbackUrl)
+		new AuthInfo(authUrl : authService.getAuthorizationUrl(null), requestToken: null, service: authService)
+	}
+
+    Token getAccessToken(OAuthService authService, Map params, Token requestToken) {
+		Verifier verifier = new Verifier(params.code as String)
+		authService.getAccessToken(requestToken, verifier)
+	}
+
+    OAuthProfile getProfile(OAuthService authService, Token accessToken) {
+        OAuthRequest request = new OAuthRequest(Verb.GET, 'https://api.github.com/user')
+        authService.signRequest(accessToken, request)
+        def response = request.send()
+
+        def user = JSON.parse(response.body)
+        new OAuthProfile(username: user.login, uid: user.id, email: user.email)
+    }
+}
