d3243500d007d2610378642e6bde38b3278f9ec2
Lucas Videla <videla.lucas@gmail.com>
2013-07-22 08:23:46 -0300

    Se retiran líneas de mongoDB

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,34 +5,18 @@ class BootStrap {
 	def fixtureLoader
 	
     def init = { servletContext ->
-		
-		
+
 		if (GrailsUtil.environment == 'development') {
-			
-//				def db = mongo.getDB('braid')
-//				
-//				db.assignment.drop()
-//				db.assignmentSolution.drop()
-//				db.homework.drop()
-//				db.role.drop()
-//				db.user.drop()
-//				db.userRole.drop()
-//	
-//				db['assignment.next_id'].drop()
-//				db['assignmentSolution.next_id'].drop()
-//				db['homework.next_id'].drop()
-//				db['role.next_id'].drop()
-//				db['user.next_id'].drop()
-//				db['userRole.next_id'].drop()
-				
+
 //			fixtureLoader.include('userFixtures')
 //			fixtureLoader.include('courseFixtures')
 //			fixtureLoader.load('questionFixtures')
 //			fixtureLoader.load('announcementFixtures')
-			//fixtureLoader.load('homeworkFixtures')
 			fixtureLoader.load('homeworkFixtures')
 		}
+
     }
+
     def destroy = {
     }
 }
