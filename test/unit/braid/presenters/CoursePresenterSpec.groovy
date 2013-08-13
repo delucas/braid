bd1838bac3a70c2abbbb32c1e538a0ed7f96636a
Lucas Videla <videla.lucas@gmail.com>
2013-08-13 19:15:32 -0300

    Added some tests

--- test/unit/braid/presenters/CoursePresenterSpec.groovy
+++ test/unit/braid/presenters/CoursePresenterSpec.groovy
@@ -1,14 +1,14 @@
 package braid.presenters
 import grails.test.mixin.*
 import grails.test.mixin.support.GrailsUnitTestMixin
-import groovy.time.TimeCategory
 import spock.lang.Specification
-import braid.AssignmentSolution
-import braid.course.Option;
+import braid.Link
+import braid.course.Option
 
 @TestMixin(GrailsUnitTestMixin)
+@Mock(Link)
 class CoursePresenterSpec extends Specification {
-	
+
 	def presenter, course
 
 	def setup() {
@@ -32,19 +32,33 @@ class CoursePresenterSpec extends Specification {
 		expect: 'hast the option'
 			presenter.hasOption(Option.assignment)
 	}
-	
+
 	void "gets banner color"() {
 		given: 'a course with banner color'
 			course << [settings: [bannerColor: '#036']]
 		expect: 'the color'
 			'#036' == presenter.bannerColor
 	}
-	
+
 	void "gets githubUsername"() {
 		given: 'a course with github username'
 			course << [settings: [githubUsername: 'lenguaje']]
 		expect: 'the github username'
 			'lenguaje' == presenter.githubUsername
 	}
-	
+
+	void "when there is no links it returns no links"() {
+		given: 'a course with no links'
+			Link.metaClass.'static'.findAllByCourse = { course, params -> [] }
+		expect: 'returns no links'
+			presenter.links.isEmpty()
+	}
+
+	void "when there are links it returns the links"() {
+		given: 'a course with no links'
+			Link.metaClass.'static'.findAllByCourse = { course, params -> [new Link()] }
+		expect: 'returns no links'
+			1 == presenter.links.size
+	}
+
 }
---
+++ test/unit/braid/presenters/reviews/CodeReviewResultsPresenterSpec.groovy
@@ -0,0 +1,56 @@
+package braid.presenters.reviews
+import grails.test.mixin.*
+import grails.test.mixin.support.GrailsUnitTestMixin
+import spock.lang.Specification
+import braid.User
+import braid.reviews.CodeReview
+import braid.reviews.CodeReviewHomework
+import braid.reviews.CodeReviewSolution
+
+@TestMixin(GrailsUnitTestMixin)
+class CodeReviewResultsPresenterSpec extends Specification {
+	
+	def presenter = new CodeReviewResultsPresenter()
+	def homework = new CodeReviewHomework()
+	def user = new User(name: 'Someone')
+	def solution = new CodeReviewSolution(user: user)
+	
+	void setup() {
+		presenter.homework = homework
+		presenter.solution = solution
+	}
+	
+	void 'when there is own review it gets own review'() {
+		given: 'a list of reviews with own solution'
+			def ownReview = new CodeReview(user: user)
+			presenter.reviews = [new CodeReview(), ownReview]
+		expect: 'presenter knows how to retrieve own review'
+			ownReview == presenter.ownReview
+	}
+	
+	void 'when there is own review it gets all but own review'() {
+		given: 'a list of reviews with own solution'
+			def ownReview = new CodeReview(user: user)
+			presenter.reviews = [new CodeReview(), ownReview]
+		expect: 'presenter knows how to retrieve own review'
+			!presenter.reviews.contains(ownReview)
+			1 == presenter.reviews.size
+	}
+	
+	void 'when there is not own review it does not get it'() {
+		given: 'a list of reviews without own solution'
+			presenter.reviews = [new CodeReview(), new CodeReview()]
+		expect: 'presenter knows how to retrieve own review'
+			null == presenter.ownReview
+	}
+	
+	void 'when there is not own review it does not get it on reviews'() {
+		given: 'a list of reviews without own solution'
+			presenter.reviews = [new CodeReview(), new CodeReview()]
+			def ownReview = new CodeReview(user: user)
+		expect: 'presenter knows how to retrieve own review'
+			!presenter.reviews.contains(ownReview)
+			2 == presenter.reviews.size
+	}
+
+}
